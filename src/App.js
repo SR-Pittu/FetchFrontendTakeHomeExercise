673ecd3adedc4536006adfc96ab37e4ebc66002e
SOBHAREDDY PITTU <102750581+SR-Pittu@users.noreply.github.com>
2025-05-17 23:20:51 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import LoginPage from './components/Auth/LoginPage';
 import DogSearchPage from './components/DogSearch/DogSearchPage';
 
@@ -21,6 +21,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
+
   );
 }
 
