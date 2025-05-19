61caf58e75cff3915ac47b6132c4082757165e31
SOBHAREDDY PITTU <102750581+SR-Pittu@users.noreply.github.com>
2025-05-18 20:42:32 -0500

    Updated packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/Auth/LoginPage';
 import DogSearchPage from './components/DogSearch/DogSearchPage';
 
